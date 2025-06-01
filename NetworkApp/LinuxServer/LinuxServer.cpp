d8e415876e2e59b2956fdd3b93e5b63b6800e836
Oscar Tillgren <tillgren.oscar@gmail.com>
2025-06-01 15:12:13 +0200

    First commit of small WIP server

---
+++ NetworkApp/LinuxServer/LinuxServer.cpp
@@ -0,0 +1,28 @@
+﻿// LinuxServer.cpp : Defines the entry point for the application.
+//
+
+#include "LinuxServer.h"
+
+#include <unistd.h>
+#include <errno.h>
+
+using namespace std;
+
+int main()
+{
+    char host_name[1024];
+
+    int ret_val = gethostname(host_name, sizeof(host_name));
+    if (ret_val == 0)
+    {
+        cout << "Hostname:" << host_name << endl;
+    }
+    else
+    {
+        cout << "Error occured: " << errno << endl;
+    }
+
+    cout << "hello";
+
+	return 0;
+}
